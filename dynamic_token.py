5fb95d0ac9ef9fd230ffc671317eb3d5c4533800
cryptofunny724 <cryptofunny724@gmail.com>
2026-06-14 18:45:59 +0200

    dynamic_token.py

--- dynamic_token.py
+++ dynamic_token.py
@@ -1,18 +1,1 @@
-contract DynamicToken:
 
-    def __init__(self):
-        self.balances = {}
-
-    def mint(self, user, amount, factor):
-        if user not in self.balances:
-            self.balances[user] = 0
-
-        adjusted = amount + (factor * 0.1)
-        self.balances[user] += adjusted
-        return adjusted
-
-    def burn(self, user, amount):
-        if self.balances.get(user, 0) >= amount:
-            self.balances[user] -= amount
-            return True
-        return False
